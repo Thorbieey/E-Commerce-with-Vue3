9520dfb1154cf573cbb9fb2cbbab33bf3a0659d8
Tobi Olajide <tobiolajide887@yahoo.com>
2023-07-27 15:46:06 +0100

    first commit

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-const product = 'Socks'
+const { createApp, ref } = Vue
+
+const app = createApp({
+  data(){
+    return {
+        product: 'Socks',
+        productDescription: 'A comfy and cozy island for your feet.',
+    }
+  }
+})
