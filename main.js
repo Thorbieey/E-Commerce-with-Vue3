92a51ffb94074856cbca57848078a5910477525b
Tobi Olajide <tobiolajide887@yahoo.com>
2023-07-27 18:44:40 +0100

    learn about the usage of v-on:click/mouseover

--- main.js
+++ main.js
@@ -3,12 +3,25 @@ const { createApp, ref } = Vue
 const app = createApp({
   data(){
     return {
+        cart:0,
         product: 'Socks',
         productDescription: 'A comfy and cozy island for your feet.',
-        imgGreen: './assets/images/socks_green.jpg',
-        imgBlue: './assets/images/socks_blue.jpg',
-        onSale: true,
-        sizes: ["Small", "Medium", "Large", "Extra-large"]
+        image: './assets/images/socks_blue.jpg',
+        inStock: true,
+        details: ['50% cotton', '30% wool', '20% polyester'],
+        variants: [
+            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+            { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+        ],
+        sizes: ["Small", "Medium", "Large", "Extra-large"],
     }
-  }
+  },
+  methods: {
+    addToCart(){
+        this.cart += 1
+    },
+    updateImage(variantImage){
+        this.image = variantImage
+    },
+  },
 })
