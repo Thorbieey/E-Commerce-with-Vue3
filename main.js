3bb96d55e03cfb00891c71767060ee2da99d03c2
Tobi Olajide <tobiolajide887@yahoo.com>
2023-07-28 00:29:16 +0100

    learnt about computed properties and how they could be used for concatenation and maths operations

--- main.js
+++ main.js
@@ -6,22 +6,35 @@ const app = createApp({
         cart:0,
         product: 'Socks',
         productDescription: 'A comfy and cozy island for your feet.',
-        image: './assets/images/socks_blue.jpg',
-        inStock: false,
+        brand: 'Vue Mastery',
         details: ['50% cotton', '30% wool', '20% polyester'],
+        selectedVariant: 0,
         variants: [
-            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-            { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+            { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
         ],
         sizes: ["Small", "Medium", "Large", "Extra-large"],
+        onSale: true,
     }
   },
   methods: {
     addToCart(){
         this.cart += 1
     },
-    updateImage(variantImage){
-        this.image = variantImage
+    updateVariant(index){
+        this.selectedVariant = index
     },
+  },
+  computed: {
+    isOnSale(){
+        return this.brand + " " + this.product + " is on sale!"
+    },
+    inStock(){
+        return this.variants[this.selectedVariant].quantity
+    },
+    image(){
+        return this.variants[this.selectedVariant].image
+    },
+
   },
 })
